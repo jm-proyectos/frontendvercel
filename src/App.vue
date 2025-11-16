80cd5947ade76865a6a502f8d90487aa986a4ecb
Jhonny <jhonnyalbertomo.com>
2025-11-15 22:13:18 -0400

    auth

--- src/App.vue
+++ src/App.vue
@@ -31,3 +31,36 @@ const cambiar=(()=>{
 </script>
 
 
+<style scoped>
+  header {
+    height: 2.5rem;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #F0F1F2;
+  }
+  .logo{
+    margin-left: 4rem;
+  }
+  .items {
+    margin-right: 4rem;
+    padding: 5px 3px;
+  }
+  .opciones{
+    display: flex;
+    align-items: center;
+  }
+  .opcion{
+    font-size: small;
+    text-decoration: none;
+    padding: 0px 10px;
+  }
+
+  .boton{
+    padding: 4px 10px;
+    border:none;
+    border-radius: 8px;
+    background-color: #609FFF;
+  }
+</style>
