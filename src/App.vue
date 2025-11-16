d52bdd5f4a1dc0188da2f4b875038809c28658bc
Jhonny <jhonnyalbertomo.com>
2025-11-15 22:43:42 -0400

    auth1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
+    <header>
+    <div class="items">
+      <nav class="opciones">
+        <router-link to="/" class="opcion">Inicio</router-link>
+        <router-link to="/" class="opcion">Dashboard</router-link>
+        <div v-if="!tokenStore.auth">
+          <router-link to="/" class="opcion boton">Iniciar Sesión</router-link>
+        </div>
+        <div v-else>
+          <button @click="cambiar()" class="opcion boton">Cerrar Sesión</button>
+        </div>
+      </nav>
+    </div>
+  </header>
     <RouterView/>
 </template>
 
 <script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import {useTokenStore} from './stores/userStore.js';
+  const tokenStore = useTokenStore();
+  import router from './router/router.js';
+  import Cookies from 'js-cookie';
+
+  const cambiar=(()=>{  
+    tokenStore.auth=false;
+    Cookies.remove('token');
+    router.push({ name: "home"});
+  });
 </script>
 
 
